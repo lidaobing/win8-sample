1cf1b6d788b9941a97c0f4893c13251befa834a0
unknown <Daobing@lidaobing-win8.(none)>
2012-05-23 21:44:19 -0700

    init version

---
+++ js/filemanager.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+    
+    function initialize() {
+          var openpicker = new Windows.Storage.Pickers.FileOpenPicker();
+              openpicker.fileTypeFilter.replaceAll([".txt", ".rtf"]);
+              openpicker.pickMultipleFilesAsync().then(function (files) {
+                      var fileNames = '';
+                   files.forEach(function (file) {
+                          fileNames += file.displayName + '<br/>';
+                          });
+                     $('#body').html(fileNames);
+                  });
+    }
+
+
+    document.addEventListener("DOMContentLoaded", initialize, false);
+
+})();
